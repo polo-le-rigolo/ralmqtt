4178a4538452080ea5b52ef5948601d7dc09471b
polo-le-rigolo <133660391+polo-le-rigolo@users.noreply.github.com>
2024-04-29 09:12:02 +0000

    Update testdos.sh

--- testdos.sh
+++ testdos.sh
@@ -8,3 +8,6 @@ while true; do
     counter=$((counter+1))
     sleep 1
 done
+
+#in another terminal, launch this command in order to check if the broker is still up : mosquitto_sub -h broker_addr -t 'test/dos' -v
+#if you don't have the mosquitto_pub/sub clients installed : sudo apt update; sudo apt install mosquitto-clients
